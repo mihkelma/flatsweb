23bdb0be799035b354822fb36c61cfdc02075a29
Mihkel Ma <mihkelmatson@gmail.com>
2018-11-14 23:37:10 +0200

    Log4j2 logger and UTF-8 support in application

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -1,6 +1,8 @@
 package config;
 
 import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +13,7 @@ import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import service.ScheduleService;
 
 @Configuration
 @EnableWebMvc
@@ -19,6 +22,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
     private ApplicationContext applicationContext;
+
+    private static final Logger logger = LogManager.getLogger(WebConfig.class);
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -36,6 +41,7 @@ public class WebConfig implements WebMvcConfigurer {
         templateResolver.setApplicationContext(applicationContext);
         templateResolver.setPrefix("/WEB-INF/views/");
         templateResolver.setSuffix(".html");
+        templateResolver.setCharacterEncoding("UTF-8");
         return templateResolver;
     }
 
@@ -60,6 +66,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void configureViewResolvers(ViewResolverRegistry registry) {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
+        resolver.setCharacterEncoding("UTF-8");
         registry.viewResolver(resolver);
     }
 
